6a33fc35c2861b566048ec0f69423fc91ae312c1
demi <627552776@qq.com>
2014-08-22 09:46:40 +0800

    change struct

---
+++ nap/common/log.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 22, 2014
+
+@author: demi
+'''
+
+class Log(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ nap/utils/shell.py
@@ -0,0 +1,8 @@
+'''
+Created on Aug 22, 2014
+
+@author: root
+'''
+
+if __name__ == '__main__':
+    pass
