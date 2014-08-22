5759b7cba2ce7b0e843759f0af4e6c6a276c6336
demi <627552776@qq.com>
2014-08-22 14:45:48 +0800

    change struct

---
+++ nap.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+'''
+Created on Aug 22, 2014
+
+@author: demi
+'''
+import sys
+
+from nap.main import main
+
+if __name__ == '__main__':
+    sys.exit(main())
